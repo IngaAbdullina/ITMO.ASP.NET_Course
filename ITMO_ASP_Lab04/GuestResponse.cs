5adfacd58b8156f4a5a210a3ff5546763a25bade
IngaAbdullina <inga.abdullina99@gmail.com>
2022-10-05 21:27:01 +0300

    Lab 4 done

---
+++ ITMO_ASP_Lab04/GuestResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+// Упражнение 1.1.Создание основы веб-сайта ASP.NET
+
+// Создание модели данных
+
+namespace AspNet
+{
+    // п.1-2
+    public class GuestResponse
+    {
+        // п.3
+        public int GuestResponseIdId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+        public DateTime Rdata { get; set; }
+
+        // п.4
+        public GuestResponse() { }
+        public GuestResponse(string name, string email, string phone, bool? willattend)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+            WillAttend = willattend;
+            Rdata = DateTime.Now;
+        }
+    }
+}
