6ff31d3de26388beb6c8c13dd1ba208c0198e7c8
IngaAbdullina <inga.abdullina99@gmail.com>
2022-10-05 21:43:15 +0300

    ASP MVC Lab 1 done

---
+++ ASP_Web_MVC_Lab01/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using ASP_Web_MVC.Models;
+using System;
+using System.Web.Mvc;
+
+namespace ASP_Web_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(string hel)
+        {
+            int hour = DateTime.Now.Hour;
+            string Greeting = ModelClass.ModelHello() + ", " + hel;
+            return Greeting;
+        }
+    }
+}
