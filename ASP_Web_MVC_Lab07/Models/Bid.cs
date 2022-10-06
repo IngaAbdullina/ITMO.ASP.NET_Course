117a5fe71caf68fcf7053bb77e76f088fb9ec5a5
IngaAbdullina <inga.abdullina99@gmail.com>
2022-10-06 15:28:10 +0300

    Lab 7 done

---
+++ ASP_Web_MVC_Lab07/Models/Bid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCreditApp1.Models
+{
+    public class Bid
+    {
+        public virtual int BidId { get; set; }
+
+        [DisplayName("Имя заявителя")]
+        [Required]
+        public virtual string Name { get; set; }
+
+        [DisplayName("Название кредита")]
+        [Required]
+        public virtual string CreditHead { get; set; }
+
+        [DisplayName("Дата подачи заявки")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yy}")]
+        public virtual DateTime bidDate { get; set; }
+    }
+}
---
+++ ASP_Web_MVC_Lab07/Models/Credit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCreditApp1.Models
+{
+    public class Credit
+    {
+        // ID кредита
+        public virtual int CreditId { get; set; }
+
+        [DisplayName("Название")]
+        [Required]
+        public virtual string Head { get; set; }
+        
+        [DisplayName("Период, на который выдается кредит")]
+        [Required]
+        public virtual int Period { get; set; }
+        
+        [DisplayName("Максимальная сумма кредита")]
+        [Required]
+        public virtual int Sum { get; set; }
+        
+        [DisplayName("Процентная ставка")]
+        [Required]
+        public virtual int Procent { get; set; }
+
+    }
+}
