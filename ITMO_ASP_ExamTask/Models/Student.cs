37d4bef9e415f7e38f9e18d656a50249ea23f1c3
IngaAbdullina <inga.abdullina99@gmail.com>
2022-10-22 14:19:06 +0300

    Exam task

---
+++ ITMO_ASP_ExamTask/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace FinalTask.Models
+{
+    public class Student
+    {
+        public virtual int StudentId { get; set; }
+        
+        public virtual string StudentName { get; set; }
+        
+        public virtual int CSDeveloping { get; set; }
+        
+        public virtual int JavaDeveloping { get; set; }
+    }
+}
---
+++ ITMO_ASP_ExamTask/Models/StudentsDbInitializer.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Entity;
+
+namespace FinalTask.Models
+{
+    public class StudentsDbInitializer: DropCreateDatabaseIfModelChanges<StudentContext>
+    {
+        protected override void Seed(StudentContext context)
+        {
+            context.Students.Add(new Student
+            {
+                StudentName = "Иванов Иван Иванович",
+                CSDeveloping = 100,
+                JavaDeveloping = 120
+            });
+            context.Students.Add(new Student
+            {
+                StudentName = "Петров Петр Петрович",
+                CSDeveloping = 90,
+                JavaDeveloping = 85
+            });
+            context.Students.Add(new Student
+            {
+                StudentName = "Сидоров Василий Львович",
+                CSDeveloping = 110,
+                JavaDeveloping = 95
+            });
+            base.Seed(context);
+        }
+    }
+}
